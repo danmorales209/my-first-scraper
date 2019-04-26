ed1f60117736c0168907b5dca18eb2e1b2630432
Dan <danmorales209@gmail.com>
2019-04-25 19:51:42 -0700

    server works

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-
-const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+
+const db = require("./models");
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -15,3 +21,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 require("./routes/api.js")(app);
+
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
