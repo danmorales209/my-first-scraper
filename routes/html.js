575fc2877066d3eab54a803e8d9b81894b53b5ef
Daniel Morales <danmorales209@gmail.com>
2019-04-26 21:24:14 -0700

    add test code for handlebars

--- routes/html.js
+++ routes/html.js
@@ -2,6 +2,25 @@ const cheerio = require('cheerio');
 
 module.exports = function (app) {
     app.get("/", function (req, res) {
-        res.render('home');
+
+        let article = [
+            {
+                title: "A",
+                link: "https://google.com",
+                _id: "1"
+            },
+            {
+                title: "B",
+                link: "https://google.com",
+                _id: "2"
+            },
+            {
+                title: "C",
+                link: "https://google.com",
+                _id: "3"
+            }
+        ];
+
+        res.render('home', { article });
     });
 }
